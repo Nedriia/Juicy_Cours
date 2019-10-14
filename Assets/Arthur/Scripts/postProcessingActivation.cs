429b30f5fe224f0a1f09d7499360caa03cf114cc
Nedriia <arthur.favre@hotmail.com>
2019-10-14 12:25:02 +0200

    PostProcess + Sprite Ghost

---
+++ Assets/Arthur/Scripts/postProcessingActivation.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PostProcessing;
+
+public class postProcessingActivation : MonoBehaviour
+{
+    public PostProcessingBehaviour test;
+
+    //public PostProcessingBehaviour postProcess;
+    private ChromaticAberrationModel.Settings Profilsetting;
+    private float chroma;
+
+    private void Start()
+    {
+        test = Camera.main.GetComponent<PostProcessingBehaviour>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            var testtest = test.profile.chromaticAberration.settings;
+            chroma += 0.5f * Time.deltaTime;
+            testtest.intensity = chroma;
+            test.profile.chromaticAberration.settings = testtest;
+        }
+        else if(Input.GetKey(KeyCode.DownArrow))
+        {
+            var testtest = test.profile.chromaticAberration.settings;
+            chroma -= 0.5f * Time.deltaTime;
+            testtest.intensity = chroma;
+            test.profile.chromaticAberration.settings = testtest;
+        }
+    }
+}
