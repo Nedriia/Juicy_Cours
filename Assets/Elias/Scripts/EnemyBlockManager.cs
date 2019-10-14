b8b1d7aaa7f8954c9e047e153a600282e55c3c66
Elias <eliasbenur@hotmail.com>
2019-10-14 11:05:36 +0200

    BlockEnemyMovement

---
+++ Assets/Elias/Scripts/EnemyBlockManager.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBlockManager : MonoBehaviour
+{
+
+    public GameObject enemy1;
+    public int numEnemies_Vertical, numEnemies_Horizontal;
+    public float spaceBetweenEnemyes_Vertical, spaceBetweenEnemyes_Horizontal;
+    public List<List<GameObject>> enemyList = new List<List<GameObject>>();
+    public float enemySpeed;
+    public float downSpaceMovement;
+    public bool goingDown;
+    public Vector3 downPosition_tmp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int y = 0; y < numEnemies_Vertical; y++)
+        {
+            List<GameObject> list_tmp = new List<GameObject>();
+            for (int x = 0; x < numEnemies_Horizontal; x++)
+            {
+                GameObject go_tmp = Instantiate(enemy1, transform.position + new Vector3(spaceBetweenEnemyes_Horizontal * x, -spaceBetweenEnemyes_Vertical * y, 0) , Quaternion.identity);
+                go_tmp.transform.parent = transform;
+                list_tmp.Add(go_tmp);
+            }
+            enemyList.Add(list_tmp);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (goingDown)
+        {
+
+        }
+    }
+
+    public IEnumerator GoToLeft()
+    {
+        GoDown();
+        yield return new WaitForSeconds(1 / enemySpeed);
+        for (int y = 0; y < numEnemies_Vertical; y++)
+        {
+            for (int x = 0; x < numEnemies_Horizontal; x++)
+            {
+                if (enemyList[x][y] != null)
+                {
+                    enemyList[x][y].GetComponent<EnemyMovement>().enemyDirection = EnemyDirection.Left;
+                }
+
+            }
+        }
+    }
+    public IEnumerator GoToRight()
+    {
+        GoDown();
+        yield return new WaitForSeconds(1/enemySpeed);
+        for (int y = 0; y < numEnemies_Vertical; y++)
+        {
+            for (int x = 0; x < numEnemies_Horizontal; x++)
+            {
+                if (enemyList[x][y] != null)
+                {
+                    enemyList[x][y].GetComponent<EnemyMovement>().enemyDirection = EnemyDirection.Right;
+                }
+            }
+        }
+    }
+
+    public void GoDown()
+    {
+        for (int y = 0; y < numEnemies_Vertical; y++)
+        {
+            for (int x = 0; x < numEnemies_Horizontal; x++)
+            {
+                if (enemyList[x][y] != null)
+                {
+                    enemyList[x][y].GetComponent<EnemyMovement>().enemyDirection = EnemyDirection.Down;
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Elias/Scripts/EnemyMovement.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum EnemyDirection
+{
+    Left,
+    Right,
+    Down
+}
+
+public class EnemyMovement : MonoBehaviour
+{
+    public float speed;
+    public EnemyDirection enemyDirection;
+
+    void Start()
+    {
+        speed = transform.parent.GetComponent<EnemyBlockManager>().enemySpeed;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        switch (enemyDirection)
+        {
+            case EnemyDirection.Down:
+                transform.position -= new Vector3(0, speed * Time.fixedDeltaTime, 0);
+                break;
+            case EnemyDirection.Left:
+                transform.position -= new Vector3(speed * Time.fixedDeltaTime, 0, 0);
+                break;
+            case EnemyDirection.Right:
+                transform.position += new Vector3(speed * Time.fixedDeltaTime, 0, 0);
+                break;
+            default:
+                break;
+        }
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "LeftLimit")
+        {
+            StartCoroutine(transform.parent.GetComponent<EnemyBlockManager>().GoToRight());
+        }else if (collision.tag == "RightLimit")
+        {
+            StartCoroutine(transform.parent.GetComponent<EnemyBlockManager>().GoToLeft());
+        }
+    }
+}
