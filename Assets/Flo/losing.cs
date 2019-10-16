da6e3e59940e5e05018d75e4bba33a0bf45f82cf
LAPTOP-JVJN19AV\flore <florentbbn@gmail.com>
2019-10-16 09:23:31 +0200

    Add my scene with tweaks

---
+++ Assets/Flo/losing.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class losing : MonoBehaviour
+{
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.tag == "Enemy")
+        {
+            Debug.Log("lose");
+        }
+    }
+    /*private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.name == "Enemy1")
+        {
+            Debug.Log("lose");
+        }
+    }*/
+
+}
---
+++ Assets/Flo/scrollBackground.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scrollBackground : MonoBehaviour
+{
+    public float speed = 0.5f;
+    Material mat;
+    public float xVelocity, yVelocity;
+    Vector2 offset;
+
+    private void Awake()
+    {
+        mat = GetComponent<Renderer>().material;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = new Vector2(xVelocity, yVelocity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mat.mainTextureOffset += offset * Time.deltaTime;
+    }
+}
